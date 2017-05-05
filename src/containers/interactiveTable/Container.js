7a29c2b75f19c53c226639837a40a21017bfe7e2
Bartosz <bartosz.kotrys@gmail.com>
2017-05-05 18:59:49 +0200

    add next part of flow typing

--- src/containers/interactiveTable/Container.js
+++ src/containers/interactiveTable/Container.js
@@ -1,3 +1,4 @@
+// @flow
 import React from 'react';
 import Table from './../../components/tables/custom/Component' ;
 import Filters from './../filtersPanel/Container';
@@ -7,20 +8,22 @@ type Props = { pets: Array<Object> };
 type State = {     
   filters: Object,
   sortedBy: string,
-  maxPrice: number
+  maxPrice: number,
+  price: number
 }; 
 
 class InteractiveTable extends React.Component<any, Props, State> {
   state: State = {
     filters: {},
     sortedBy: "",
-    maxPrice: 0
+    maxPrice: 0,
+    price: 0
   }
-  get columnsNames() {
+  get columnsNames() : Array<string> {
     const pets = this.props.pets;
     return pets.length ? Object.keys(pets[0]) : [];
   }
-  get pets() { 
+  get pets() : Array<Object> { 
     return this.getPetsIncludedInPrice(
             this.getSortedPets(
               this.getVisiblePets(
@@ -29,44 +32,44 @@ class InteractiveTable extends React.Component<any, Props, State> {
               )
             );
   }
-  getInitialFiltersState() {
+  getInitialFiltersState() : Object {
     return this.props.pets.reduce(function(map, petData) {
         map[petData.animal] = true;
         return map;
     }, {});
   }
-  getVisiblePets(pets) {
+  getVisiblePets(pets: Array<Object>) : Array<Object> {
     return pets.filter((pet) => this.state.filters[pet.animal]);
   }
-  getSortedPets(pets) {
+  getSortedPets(pets: Array<Object>) : Array<Object> {
     const isAscending = this.state.sortedBy.endsWith("+"); 
     const sortedBy = this.state.sortedBy.slice(0, -1);
     return pets.sort((a, b) => isAscending ?
       (a[sortedBy] - b[sortedBy]) : (b[sortedBy] - a[sortedBy]));
   }
-  getPetsIncludedInPrice(pets) {
+  getPetsIncludedInPrice(pets: Array<Object>) : Array<Object> {
     return pets.filter((pet) => pet.price <= this.state.price);
   }
-  componentDidMount() {
+  componentDidMount() : void {
     this.setState({
       filters: this.getInitialFiltersState(),
       price: 1000 // should be get from service    
     })
   }
-  filterByAnimalType = (filterName, isChecked) => {
+  filterByAnimalType = (filterName: string, isChecked: boolean) : void => {
     this.setState({
       filters: Object.assign(
         {}, this.state.filters, { [filterName]: isChecked }
       )
     })
   }
-  filterByPrice = (currentPrice) => {
+  filterByPrice = (currentPrice: number) : void => {
     this.setState({ price: currentPrice })
   }
-  sortBy = (filterName) => {
+  sortBy = (filterName: string) : void => {
     this.setState({ sortedBy: filterName })
   }
-  render() { 
+  render() : React$Element<any> { 
     return (
       <section>
         { this.props.pets.length ?
